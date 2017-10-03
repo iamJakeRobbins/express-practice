9034aec1d53299b96756f745f0e6f9c8b5823804
Jake Robbins <jrobbinsd@gmail.com>
2017-10-03 09:47:27 -0600

    initial commit

---
+++ practice6/index.js
@@ -0,0 +1,9 @@
+var express = require('express')
+var app = express()
+
+app.listen(3000, function () {
+	console.log('Example app listening on port 3000!')
+})
+app.get('/', function (req, res) {
+  res.send('This is the last one!!!!')
+})
